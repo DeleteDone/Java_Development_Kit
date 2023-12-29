6f2287eba1ac80fb883994b03759ecd04edf25d8
KiTSune MiDSun <118630098+DeleteDone@users.noreply.github.com>
2023-12-29 15:51:48 +0300

    Delete seminar_1/game_window directory

--- seminar_1/game_window/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.seminar_1.game_window;
-
-public class Main {
-
-    public static void main(String[] args) {
-        new GameWindow();
-        System.out.println("Method main() is over.");
-    }
-}
