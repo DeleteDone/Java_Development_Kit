0180d4bd4f56ddb52c691991b8c9d166d1150e6f
KiTSune MiDSun <118630098+DeleteDone@users.noreply.github.com>
2024-01-21 13:02:53 +0300

    Update Main.java

--- seminar_4/Project_Direcroty/src/main/java/ru/geekbrains/Main.java
+++ seminar_4/Project_Direcroty/src/main/java/ru/geekbrains/Main.java
@@ -23,10 +23,10 @@ public class Main {
 
         EmployeeDirectory dir = new EmployeeDirectory();
 
-        Employee employee1 = new Employee(1,666333,"Dima",5);
+        Employee employee1 = new Employee(1,666333,"Dmitry",5);
         Employee employee2 = new Employee(2,777,"Anna",1);
-        Employee employee3 = new Employee(3,987654,"Oleg",7);
-        Employee employee4 = new Employee(4,123123,"Dima",3);
+        Employee employee3 = new Employee(3,987654,"Nikita",7);
+        Employee employee4 = new Employee(4,123123,"Dmitry",3);
 
         dir.addEmployee(employee1);
         dir.addEmployee(employee2);
@@ -36,22 +36,22 @@ public class Main {
         System.out.println();
 
 //      1. Добавить метод, который ищет сотрудника по стажу (может быть список)
-        System.out.println("1. Поиск сотрудника по стажу. Должен вывести сотрудника Dima: ");
+        System.out.println("1. Поиск сотрудника по стажу. Должен вывести сотрудника Dmitry: ");
         System.out.println(dir.searchByExperience(5));
 
 //      2. Добавить метод, который выводит номер телефона сотрудника по имени (может быть список)
         System.out.println();
-        System.out.println("2. Поиск сотрудника по номеру телефона. Должен вывести сотрудников с именем Dima: ");
-        System.out.println(dir.getPhoneByName("Dima"));
+        System.out.println("2. Поиск сотрудника по номеру телефона. Должен вывести сотрудников с именем Dmitry: ");
+        System.out.println(dir.getPhoneByName("Dmitry"));
 
 //      3. Добавить метод, который ищет сотрудника по табельному номеру
         System.out.println();
-        System.out.println("3. Поиск сотрудника по табельному номеру. Должен вывести сотрудника с именем Oleg: ");
+        System.out.println("3. Поиск сотрудника по табельному номеру. Должен вывести сотрудника с именем Nikita: ");
         System.out.println(dir.searchByServiceNumber(3));
 
 //      4. Добавить метод добавление нового сотрудника в справочник
         System.out.println();
-        System.out.println("4. Добавляем сотрудника с именем Pavel: ");
+        System.out.println("4. Добавляем сотрудника с именем Sergey: ");
         Employee employee5 = new Employee(5,111,"Pavel",10);
         dir.addEmployee(employee5);
 
